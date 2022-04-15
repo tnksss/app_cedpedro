7e45bcca24660002ba8e55a50c0157e0e8d08ebe
tnksss <robsons@alunos.utfpr.edu.br>
2022-04-15 15:41:48 -0300

    sidebar update for user actions

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -31,14 +31,16 @@
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
+            <i class="fas fa-solid fa-users"></i>
+            <span>Usuários</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
+                <h6 class="collapse-header">Ações:</h6>
+                <a class="collapse-item" href="buttons.html"><i class="fas fa-solid fa-plus"></i>
+                    <span>Adicionar</span></a>
+                <a class="collapse-item" href="cards.html"><i class="fas fa-solid fa-list"></i>
+                    <span>Listar</span></a>
             </div>
         </div>
     </li>
